84f7c250a1f94c66ff32dc85a439faba0b00ad1f
varo6 <alvarohr2003@gmail.com>
2024-02-11 11:52:39 +0100

    Add pyMerger

---
+++ pyMerger.py
@@ -0,0 +1,25 @@
+import os
+from pypdf import PdfWriter
+
+##Get current path as string, head to the pdfs folder
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path,"pdfs")
+
+##Get array of files located in the path
+mylist = os.listdir(path);
+
+##Merge pdf files
+merger = PdfWriter()
+
+for pdf in mylist:
+    currentFile = os.path.join(path, pdf)
+    print("Appending " +currentFile+ " to the merged pdf")
+    merger.append(r""+currentFile)
+
+merger.write(os.path.join(path,"mergedpdf.pdf"))
+
+##Check if mergedpdf was created and if it's not null
+currentFile = os.path.join(path, "mergedpdf.pdf")
+if currentFile is not None:
+    print("Merge completed!")
+merger.close()
---
+++ readme.md
@@ -0,0 +1,6 @@
+# pypdf-scripts
+
+Some scripts using pypdf for daily use
+
+Current scripts:
+	- pyMerger: A script made for merging pdfs located in "pdfs" folder, will create a merged pdf inside the folder.
