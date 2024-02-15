8a04ab1898a6e677d0985d9ef7f667422edd9d4d
varo6 <alvarohr2003@gmail.com>
2024-02-15 17:40:45 +0100

    Change results folder

--- pyMerger.py
+++ pyMerger.py
@@ -2,25 +2,25 @@ import os
 from pypdf import PdfWriter
 
 ##Get current path as string, head to the pdfs folder
-path = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(path,"pdfs")
+parent_path = os.path.dirname(os.path.abspath(__file__))
+pdfs_path = os.path.join(parent_path,"pdfs")
 
 ##Get array of files located in the path
-mylist = os.listdir(path);
+mylist = os.listdir(pdfs_path);
 
 ##Merge pdf files
 merger = PdfWriter()
 
 for pdf in mylist:
     if pdf.lower().endswith('.pdf'):
-        currentFile = os.path.join(path, pdf)
+        currentFile = os.path.join(pdfs_path, pdf)
         print("Appending " +currentFile+ " to the merged pdf")
         merger.append(r""+currentFile)
 
-merger.write(os.path.join(path,"mergedpdf.pdf"))
+currentFile = os.path.join(parent_path,"saved results","mergedpdf.pdf")
+merger.write(currentFile)
 
 ##Check if mergedpdf was created and if it's not null
-currentFile = os.path.join(path, "mergedpdf.pdf")
 if currentFile is not None:
     print("Merge completed!")
 merger.close()
