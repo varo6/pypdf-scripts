7a7c197f12f510e49f982d3f247639bbff3c8b7f
varo6 <alvarohr2003@gmail.com>
2024-02-23 17:45:25 +0100

    change project architecture
    started as a "one file script". In order to add more utilities and scripts I changed it to a main.py & packages architecture.

--- pdfs/add-pdfs-here.md
+++
@@ -1,3 +0,0 @@
-This folder contains your pdfs or will output your pdfs using the scripts. 
-
-For pyMerger use just copy here all your pdfs desired to merge and will generate a "mergedpdf.pdf" file
---
+++ src/pyMerger.py
@@ -0,0 +1,43 @@
+import os
+from pypdf import PdfWriter
+
+class PyMerger:
+    def __init__(self):
+        print("*** Start PyMerger ***")
+ 
+        ##Get current path as string, head to the pdfs folder
+        parent_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        pdfs_path = os.path.join(parent_path,"pdfs")
+        results_path = os.path.join(parent_path,"saved results")
+        counter = 0
+
+        ##Get array of files located in the path
+        mylist = os.listdir(pdfs_path)
+
+        ##Merge pdf files
+        merger = PdfWriter()
+
+        ##Add all pdf files to the PdfWriter() Object
+        for pdf in mylist:
+            if pdf.lower().endswith('.pdf'):
+                currentFile = os.path.join(pdfs_path, pdf)
+                print("Appending " +currentFile+ " to the merged pdf")
+                merger.append(r""+currentFile)
+
+        ##Check if mergedpdf exists and create the new pdf
+        mylist = os.listdir(results_path)
+        for pdf in mylist:
+            if pdf.lower().startswith("mergedpdf"):
+                counter = counter + 1
+        if counter != 0:
+            print("There are existing merged pdf files. Adding ID")
+            currentFile = os.path.join(results_path,"mergedpdf" + str(counter)+".pdf")
+            merger.write(currentFile)  
+        else:
+            currentFile = os.path.join(results_path,"mergedpdf.pdf")
+            merger.write(currentFile)
+
+        ##Check if mergedpdf was created and if it's not null
+        if currentFile is not None:
+            print("Merge completed!")
+        merger.close()
