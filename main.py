5c2b9c927add88f2382d4cda37aa5572b4c9f28a
varo6 <alvarohr2003@gmail.com>
2024-03-05 22:44:14 +0100

    add base flet app

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src import PyMerger
-def main():
+#Flet will be the interface package
+import flet as ft 
+
+#Will recieve page as a parameter. If not, create an ft.Page class
+def main(page: ft.Page):
+    pass
+
     try:
-        merger = PyMerger()
+        merger = PyMerger()   
     except FileNotFoundError as e:
         print("Error:", e)
 
 if __name__=="__main__":
-    main()
+    ft.app(target=main)
